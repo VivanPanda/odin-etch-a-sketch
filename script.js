5be69f0e7b4c4a13cd9676932d6adba2b6b69ea5
Vivan <vivanpanda13@gmail.com>
2023-04-29 12:03:14 +0400

    Implement hover effect for change color

--- script.js
+++ script.js
@@ -2,11 +2,20 @@ gridContainer = document.querySelector('.grid-container');
 
 function createGrid (rows, cols) {
     for (let i = 0; i < (rows * cols); i++) {
-        const gridBox = document.createElement('div');
-        gridBox.classList.add("grid-box");
-        gridContainer.appendChild(gridBox);
-
+        let gridBoxCreation = document.createElement('div');
+        gridBoxCreation.classList.add("grid-box");
+        gridContainer.appendChild(gridBoxCreation);
     }
 }
 
-createGrid(16,16)
+function makeColorBlack() {
+    const gridBox = document.querySelectorAll('.grid-box');
+    for (let j = 0; j < (gridBox.length); j++) {
+        gridBox[j].addEventListener('mouseover', () => {
+            gridBox[j].classList.add('black');
+        })
+    };
+}
+
+createGrid(16,16);
+makeColorBlack();
